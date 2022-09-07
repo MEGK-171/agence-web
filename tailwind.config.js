7131b7bec2e632e86593dbb75ef0e4c7421a9dc3
KillianE2C77 <sta.killian.benigno@e2c77.org>
2022-09-07 16:54:35 +0200

    small uodate

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      custom1: "#78dcca",
+      customBlack: {
+        blacklight: "#272727",
+        blacklightdark: "#1C1C1C",
+      },
+    },
     extend: {},
   },
   plugins: [],
-}
+};
