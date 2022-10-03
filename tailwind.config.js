4558ce3a34b6b23f9e1b36132364c0ae2fdd01d0
gorkem-38 <gorrkeem38li@gmail.com>
2022-10-03 20:17:34 +0200

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ const colors = require("tailwindcss/colors");
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'bg-silver': "#C0C0C0"
+      },
+
+      backgroundImage: {
+        'bg-img': "url(../../src/assets/img/responsive.png)"
+      }
+    },
   },
   plugins: [],
 };
