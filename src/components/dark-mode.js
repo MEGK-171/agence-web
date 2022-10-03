e8434831bb1086d2c788af93b196c50bbe80b9d6
emirgedikli <emirwtaha@gmail.com>
2022-10-03 11:44:41 +0200

    all (dev-emir)

--- src/components/dark-mode.js
+++ src/components/dark-mode.js
@@ -1,15 +1,15 @@
-const btn = document.getElementById("dark-mode");
-const body = document.querySelector("body");
+// const btn = document.getElementById("dark-mode");
+// const body = document.querySelector("body");
 
 
 
-const darkMode = () => {
-  body.classList.toggle("bg-customBlack-blacklightdark");
+// const darkMode = () => {
+//   body.classList.toggle("bg-customBlack-blacklightdark");
   
-  body.classList.toggle("text-white");
-};
+//   body.classList.toggle("text-white");
+// };
 
-btn.addEventListener("click", darkMode);
+// btn.addEventListener("click", darkMode);
 
-export default darkMode;
+// export default darkMode;
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,20 +5,13 @@ const colors = require("tailwindcss/colors");
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    colors: {
-      custom1: "#78dcca",
-      customBlack: {
-        blacklight: "#272727",
-        blacklightdark: "#1C1C1C",
-        blackdark: "#202020",
+    theme: {
+      extend: {
       },
-      customGreen: {
-        greenlight: "#6BAE8A",
+      colors: {
+        ...colors,
       },
-      white: colors.white,
     },
-    extend: {},
+    plugins: [],
   },
-  plugins: [],
 };
-
