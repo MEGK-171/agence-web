b8ad4bbe4e48e7404ed406f480755b26b720aa59
emirgedikli <emirwtaha@gmail.com>
2022-10-04 11:45:43 +0200

    footer finished -EmirGedikli

--- src/app.js
+++ src/app.js
@@ -1,10 +1,2 @@
 import "./assets/css/styles.css";
-import "./assets/font-awesome-4.7.0/css/font-awesome.min.css"
-
-// import Menu from "./components/menu";
-// Menu();
-
-// import darkMode from "./components/dark-mode";
-// darkMode();
-
-// let body = document.querySelector("body");
+import "./assets/font-awesome-4.7.0/css/font-awesome.min.css";
