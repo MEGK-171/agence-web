5942d02d92cfe0feb930c425a1d0ac354c010eb8
Marcelo1Marques <marcelo99david@gmail.com>
2022-10-05 12:00:26 +0200

    update

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,18 +1,17 @@
 const menuMobile = document.getElementById("menu-mobile");
 const navMobile = document.getElementById("nav-mobile");
 const root = document.getElementById("root");
-const slide = document.getElementById("slide")
 
 const Menu = () => {
     const showMenu = () => {
         navMobile.classList.toggle("hidden");
+        navMobile.classList.toggle("bg-green-100");
     };
     const closeMenu = () => {
         navMobile.classList.contains("hidden") ? console.log("true") : navMobile.classList.add("hidden"); 
     };
     menuMobile.addEventListener("click", showMenu);
     root.addEventListener("click", closeMenu);
-    slide.classList.toggle("slide");
 };
 
 export default Menu;
