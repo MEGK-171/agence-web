7538c827f2cbf3745fe29d68adfb7e2128154770
emirgedikli <emirwtaha@gmail.com>
2022-10-05 12:12:15 +0200

    all in main

--- src/components/menu.js
+++ src/components/menu.js
@@ -5,12 +5,13 @@ const root = document.getElementById("root");
 const Menu = () => {
     const showMenu = () => {
         navMobile.classList.toggle("hidden");
-        navMobile.classList.toggle("bg-green-100");
     };
+
     const closeMenu = () => {
         navMobile.classList.contains("hidden") ? console.log("true") : navMobile.classList.add("hidden"); 
     };
     menuMobile.addEventListener("click", showMenu);
+
     root.addEventListener("click", closeMenu);
 };
 
