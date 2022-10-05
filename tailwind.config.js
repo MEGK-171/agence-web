98bb43b754fb1f63ce555e05d1b8c897fca23fd6
emirgedikli <emirwtaha@gmail.com>
2022-10-05 16:12:41 +0200

    branch emir ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-/** @type {import('tailwindcss').Config} */
-
-const colors = require("tailwindcss/colors");
-
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      backgroundColor: {
-        'silver': "#C0C0C0"
-      },
-
-      backgroundImage: {
-        'responsive': "url('../img/responsive.png')",
-        'paper': "url(../img/paper.svg)",
-        'moon': "url(../img/moon.svg)",
-        'intro': "url(../img/intro.jpg)"
-      }
-    },
-  },
-  plugins: [],
-};
-
-=======
 /** @type {import('tailwindcss').Config} */
 
 const colors = require("tailwindcss/colors");
@@ -45,4 +20,3 @@ module.exports = {
   },
   plugins: [],
 };
->>>>>>> dev-gorkem
